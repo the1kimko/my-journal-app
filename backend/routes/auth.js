eb11e08aae8900cab4eebee47ebb89349f537c58
KomfortKimko <komfortkimk@gmail.com>
2025-05-03 15:10:33 +0300

    changed root directory for node.js backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('../node_modules/bcryptjs/umd');
 const { SECRET } = require('../middleware/auth');
 
 const router = express.Router();
